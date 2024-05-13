25328aaf07b5b59b27dbe680203a050359d3b394
yann-supinfo <125275466+yann-supinfo@users.noreply.github.com>
2024-05-13 22:30:20 +0200

    Delete messenger_contrat_inversion directory

--- messenger_contrat_inversion/src/main/java/com/supinfo/messenger/service/InvoiceServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.supinfo.messenger.service;
-
-import com.supinfo.messenger.entity.Invoice;
-import com.supinfo.messenger.repository.InvoiceRepositoryInterface;
-
-public interface InvoiceServiceInterface {
-    void createInvoice(Invoice invoice);
-    void setInvoiceRepository(InvoiceRepositoryInterface invoiceRepository);
-}
